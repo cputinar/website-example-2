36205ce61c54b6598b589b00afac6b29ca289c6e
Corina Putinar <cputinar@gmail.com>
2018-07-07 18:00:45 +0000

    Initial blog commit

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+         
+  # link to Posts
+  has_many :posts
+end
